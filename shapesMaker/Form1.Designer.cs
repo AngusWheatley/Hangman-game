f919a8c1254379b0bb2d68e57ef66ccc99622df6
AngusWheatley <angus.wheatley2@education.nsw.gov.au>
2022-08-08 17:46:46 +1000

    Update 8/08/2022

--- shapesMaker/Form1.Designer.cs
+++ shapesMaker/Form1.Designer.cs
@@ -28,21 +28,394 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnNewWord = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnInstructions = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnE = new System.Windows.Forms.Button();
+            this.btnF = new System.Windows.Forms.Button();
+            this.btnG = new System.Windows.Forms.Button();
+            this.btnH = new System.Windows.Forms.Button();
+            this.btnI = new System.Windows.Forms.Button();
+            this.btnJ = new System.Windows.Forms.Button();
+            this.btnK = new System.Windows.Forms.Button();
+            this.btnL = new System.Windows.Forms.Button();
+            this.btnM = new System.Windows.Forms.Button();
+            this.btnN = new System.Windows.Forms.Button();
+            this.btnO = new System.Windows.Forms.Button();
+            this.btnP = new System.Windows.Forms.Button();
+            this.btnQ = new System.Windows.Forms.Button();
+            this.btnR = new System.Windows.Forms.Button();
+            this.btnS = new System.Windows.Forms.Button();
+            this.btnT = new System.Windows.Forms.Button();
+            this.btnU = new System.Windows.Forms.Button();
+            this.btnV = new System.Windows.Forms.Button();
+            this.btnW = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnY = new System.Windows.Forms.Button();
+            this.btnZ = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnNewWord
+            // 
+            this.btnNewWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnNewWord.Location = new System.Drawing.Point(9, 566);
+            this.btnNewWord.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNewWord.Name = "btnNewWord";
+            this.btnNewWord.Size = new System.Drawing.Size(146, 37);
+            this.btnNewWord.TabIndex = 0;
+            this.btnNewWord.Text = "New Word";
+            this.btnNewWord.UseVisualStyleBackColor = true;
+            this.btnNewWord.Click += new System.EventHandler(this.btnNewWord_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.35F);
+            this.lblScore.Location = new System.Drawing.Point(17, 24);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(52, 15);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score: 0";
+            this.lblScore.Click += new System.EventHandler(this.lblScore_Load);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.btnStart.Location = new System.Drawing.Point(20, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(269, 65);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnInstructions
+            // 
+            this.btnInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.btnInstructions.Location = new System.Drawing.Point(20, 135);
+            this.btnInstructions.Name = "btnInstructions";
+            this.btnInstructions.Size = new System.Drawing.Size(269, 134);
+            this.btnInstructions.TabIndex = 3;
+            this.btnInstructions.Text = "HOW TO PLAY";
+            this.btnInstructions.UseVisualStyleBackColor = true;
+            this.btnInstructions.Click += new System.EventHandler(this.btnInstructions_Click);
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(597, 505);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(50, 34);
+            this.btnA.TabIndex = 4;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(653, 505);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(50, 34);
+            this.btnB.TabIndex = 4;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = true;
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(709, 505);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(50, 34);
+            this.btnC.TabIndex = 4;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(765, 505);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(50, 34);
+            this.btnD.TabIndex = 4;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = true;
+            // 
+            // btnE
+            // 
+            this.btnE.Location = new System.Drawing.Point(821, 505);
+            this.btnE.Name = "btnE";
+            this.btnE.Size = new System.Drawing.Size(50, 34);
+            this.btnE.TabIndex = 4;
+            this.btnE.Text = "E";
+            this.btnE.UseVisualStyleBackColor = true;
+            // 
+            // btnF
+            // 
+            this.btnF.Location = new System.Drawing.Point(877, 505);
+            this.btnF.Name = "btnF";
+            this.btnF.Size = new System.Drawing.Size(50, 34);
+            this.btnF.TabIndex = 4;
+            this.btnF.Text = "F";
+            this.btnF.UseVisualStyleBackColor = true;
+            // 
+            // btnG
+            // 
+            this.btnG.Location = new System.Drawing.Point(933, 505);
+            this.btnG.Name = "btnG";
+            this.btnG.Size = new System.Drawing.Size(50, 34);
+            this.btnG.TabIndex = 4;
+            this.btnG.Text = "G";
+            this.btnG.UseVisualStyleBackColor = true;
+            // 
+            // btnH
+            // 
+            this.btnH.Location = new System.Drawing.Point(597, 545);
+            this.btnH.Name = "btnH";
+            this.btnH.Size = new System.Drawing.Size(50, 34);
+            this.btnH.TabIndex = 4;
+            this.btnH.Text = "H";
+            this.btnH.UseVisualStyleBackColor = true;
+            // 
+            // btnI
+            // 
+            this.btnI.Location = new System.Drawing.Point(653, 545);
+            this.btnI.Name = "btnI";
+            this.btnI.Size = new System.Drawing.Size(50, 34);
+            this.btnI.TabIndex = 4;
+            this.btnI.Text = "I";
+            this.btnI.UseVisualStyleBackColor = true;
+            // 
+            // btnJ
+            // 
+            this.btnJ.Location = new System.Drawing.Point(709, 545);
+            this.btnJ.Name = "btnJ";
+            this.btnJ.Size = new System.Drawing.Size(50, 34);
+            this.btnJ.TabIndex = 4;
+            this.btnJ.Text = "J";
+            this.btnJ.UseVisualStyleBackColor = true;
+            // 
+            // btnK
+            // 
+            this.btnK.Location = new System.Drawing.Point(765, 545);
+            this.btnK.Name = "btnK";
+            this.btnK.Size = new System.Drawing.Size(50, 34);
+            this.btnK.TabIndex = 4;
+            this.btnK.Text = "K";
+            this.btnK.UseVisualStyleBackColor = true;
+            // 
+            // btnL
+            // 
+            this.btnL.Location = new System.Drawing.Point(821, 545);
+            this.btnL.Name = "btnL";
+            this.btnL.Size = new System.Drawing.Size(50, 34);
+            this.btnL.TabIndex = 4;
+            this.btnL.Text = "L";
+            this.btnL.UseVisualStyleBackColor = true;
+            // 
+            // btnM
+            // 
+            this.btnM.Location = new System.Drawing.Point(877, 545);
+            this.btnM.Name = "btnM";
+            this.btnM.Size = new System.Drawing.Size(50, 34);
+            this.btnM.TabIndex = 4;
+            this.btnM.Text = "M";
+            this.btnM.UseVisualStyleBackColor = true;
+            // 
+            // btnN
+            // 
+            this.btnN.Location = new System.Drawing.Point(933, 545);
+            this.btnN.Name = "btnN";
+            this.btnN.Size = new System.Drawing.Size(50, 34);
+            this.btnN.TabIndex = 4;
+            this.btnN.Text = "N";
+            this.btnN.UseVisualStyleBackColor = true;
+            // 
+            // btnO
+            // 
+            this.btnO.Location = new System.Drawing.Point(625, 585);
+            this.btnO.Name = "btnO";
+            this.btnO.Size = new System.Drawing.Size(50, 34);
+            this.btnO.TabIndex = 4;
+            this.btnO.Text = "O";
+            this.btnO.UseVisualStyleBackColor = true;
+            // 
+            // btnP
+            // 
+            this.btnP.Location = new System.Drawing.Point(681, 585);
+            this.btnP.Name = "btnP";
+            this.btnP.Size = new System.Drawing.Size(50, 34);
+            this.btnP.TabIndex = 4;
+            this.btnP.Text = "P";
+            this.btnP.UseVisualStyleBackColor = true;
+            // 
+            // btnQ
+            // 
+            this.btnQ.Location = new System.Drawing.Point(737, 585);
+            this.btnQ.Name = "btnQ";
+            this.btnQ.Size = new System.Drawing.Size(50, 34);
+            this.btnQ.TabIndex = 4;
+            this.btnQ.Text = "Q";
+            this.btnQ.UseVisualStyleBackColor = true;
+            // 
+            // btnR
+            // 
+            this.btnR.Location = new System.Drawing.Point(793, 585);
+            this.btnR.Name = "btnR";
+            this.btnR.Size = new System.Drawing.Size(50, 34);
+            this.btnR.TabIndex = 4;
+            this.btnR.Text = "R";
+            this.btnR.UseVisualStyleBackColor = true;
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(849, 585);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(50, 34);
+            this.btnS.TabIndex = 4;
+            this.btnS.Text = "S";
+            this.btnS.UseVisualStyleBackColor = true;
+            // 
+            // btnT
+            // 
+            this.btnT.Location = new System.Drawing.Point(905, 585);
+            this.btnT.Name = "btnT";
+            this.btnT.Size = new System.Drawing.Size(50, 34);
+            this.btnT.TabIndex = 4;
+            this.btnT.Text = "T";
+            this.btnT.UseVisualStyleBackColor = true;
+            // 
+            // btnU
+            // 
+            this.btnU.Location = new System.Drawing.Point(625, 625);
+            this.btnU.Name = "btnU";
+            this.btnU.Size = new System.Drawing.Size(50, 34);
+            this.btnU.TabIndex = 4;
+            this.btnU.Text = "U";
+            this.btnU.UseVisualStyleBackColor = true;
+            // 
+            // btnV
+            // 
+            this.btnV.Location = new System.Drawing.Point(681, 625);
+            this.btnV.Name = "btnV";
+            this.btnV.Size = new System.Drawing.Size(50, 34);
+            this.btnV.TabIndex = 4;
+            this.btnV.Text = "V";
+            this.btnV.UseVisualStyleBackColor = true;
+            // 
+            // btnW
+            // 
+            this.btnW.Location = new System.Drawing.Point(737, 625);
+            this.btnW.Name = "btnW";
+            this.btnW.Size = new System.Drawing.Size(50, 34);
+            this.btnW.TabIndex = 4;
+            this.btnW.Text = "W";
+            this.btnW.UseVisualStyleBackColor = true;
+            // 
+            // btnX
+            // 
+            this.btnX.Location = new System.Drawing.Point(793, 625);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(50, 34);
+            this.btnX.TabIndex = 4;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = true;
+            // 
+            // btnY
+            // 
+            this.btnY.Location = new System.Drawing.Point(849, 625);
+            this.btnY.Name = "btnY";
+            this.btnY.Size = new System.Drawing.Size(50, 34);
+            this.btnY.TabIndex = 4;
+            this.btnY.Text = "Y";
+            this.btnY.UseVisualStyleBackColor = true;
+            // 
+            // btnZ
+            // 
+            this.btnZ.Location = new System.Drawing.Point(905, 625);
+            this.btnZ.Name = "btnZ";
+            this.btnZ.Size = new System.Drawing.Size(50, 34);
+            this.btnZ.TabIndex = 4;
+            this.btnZ.Text = "Z";
+            this.btnZ.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1084, 761);
+            this.Controls.Add(this.btnN);
+            this.Controls.Add(this.btnZ);
+            this.Controls.Add(this.btnT);
+            this.Controls.Add(this.btnM);
+            this.Controls.Add(this.btnY);
+            this.Controls.Add(this.btnG);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.btnL);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnF);
+            this.Controls.Add(this.btnR);
+            this.Controls.Add(this.btnK);
+            this.Controls.Add(this.btnW);
+            this.Controls.Add(this.btnE);
+            this.Controls.Add(this.btnQ);
+            this.Controls.Add(this.btnJ);
+            this.Controls.Add(this.btnV);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnP);
+            this.Controls.Add(this.btnU);
+            this.Controls.Add(this.btnI);
+            this.Controls.Add(this.btnO);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnH);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnInstructions);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnNewWord);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnNewWord;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnInstructions;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnE;
+        private System.Windows.Forms.Button btnF;
+        private System.Windows.Forms.Button btnG;
+        private System.Windows.Forms.Button btnH;
+        private System.Windows.Forms.Button btnI;
+        private System.Windows.Forms.Button btnJ;
+        private System.Windows.Forms.Button btnK;
+        private System.Windows.Forms.Button btnL;
+        private System.Windows.Forms.Button btnM;
+        private System.Windows.Forms.Button btnN;
+        private System.Windows.Forms.Button btnO;
+        private System.Windows.Forms.Button btnP;
+        private System.Windows.Forms.Button btnQ;
+        private System.Windows.Forms.Button btnR;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Button btnT;
+        private System.Windows.Forms.Button btnU;
+        private System.Windows.Forms.Button btnV;
+        private System.Windows.Forms.Button btnW;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnY;
+        private System.Windows.Forms.Button btnZ;
     }
 }
 
--- shapesMaker/Form1.cs
+++ shapesMaker/Form1.cs
@@ -12,12 +12,181 @@ namespace shapesMaker
 {
     public partial class Form1 : Form
     {
+        bool start = false;
+        const string path = "wordList.txt";
+        string randomWord = "";
+        int score = 0;
+
         public Form1()
         {
             InitializeComponent();
 
-            this.Width = 500;
-            this.Height = 500;
+            this.Width = 1100;
+            this.Height = 800;
+
+            btnNewWord.Hide();
+            btnNewWord.Width = 160;
+            btnNewWord.Height = 50;
+            btnNewWord.Left = ClientSize.Width - 1050;
+            btnNewWord.Top = ClientSize.Height - 80;
+
+            btnStart.Width = 300;
+            btnStart.Height = 150;
+            btnStart.Left = ClientSize.Width - 900;
+            btnStart.Top = ClientSize.Height - 550;
+
+            btnInstructions.Width = 300;
+            btnInstructions.Height = 150;
+            btnInstructions.Left = ClientSize.Width - 500;
+            btnInstructions.Top = ClientSize.Height - 550;
+
+
+            lblScore.Hide();
+            lblScore.Width = 50;
+            lblScore.Height = 20;
+
+            btnA.Hide();
+            btnB.Hide();
+            btnC.Hide();
+            btnD.Hide();
+            btnE.Hide();
+            btnF.Hide();
+            btnG.Hide();
+            btnH.Hide();
+            btnI.Hide();
+            btnJ.Hide();
+            btnK.Hide();
+            btnL.Hide();
+            btnM.Hide();
+            btnN.Hide();
+            btnO.Hide();
+            btnP.Hide();
+            btnQ.Hide();
+            btnR.Hide();
+            btnS.Hide();
+            btnT.Hide();
+            btnU.Hide();
+            btnV.Hide();
+            btnW.Hide();
+            btnX.Hide();
+            btnY.Hide();
+            btnZ.Hide();
+
+            {
+                /*
+                btnA.Hide();
+                btnA.Width = 50;
+                btnA.Height = 34;
+                btnA.Left = 597;
+                btnA.Top = 505;
+
+                btnB.Hide();
+                btnB.Width = 50;
+                btnB.Height = 34;
+                btnB.Left = 653;
+                btnB.Top = 505;
+
+                btnC.Hide();
+                btnC.Width = 50;
+                btnC.Height = 34;
+                btnC.Left = 709;
+                btnC.Top = 505;
+
+                btnD.Hide();
+                btnD.Width = 50;
+                btnD.Height = 34;
+                btnD.
+
+                btnE.Hide();
+                btnE.Width = 50;
+                btnE.Height = 34;
+
+                btnF.Hide();
+                btnF.Width = 50;
+                btnF.Height = 34;
+
+                btnG.Hide();
+                btnG.Width = 50;
+                btnG.Height = 34;
+
+                btnH.Hide();
+                btnH.Width = 50;
+                btnH.Height = 34;
+
+                btnI.Hide();
+                btnI.Width = 50;
+                btnI.Height = 34;
+
+                btnJ.Hide();
+                btnJ.Width = 50;
+                btnJ.Height = 34;
+
+                btnK.Hide();
+                btnK.Width = 50;
+                btnK.Height = 34;
+
+                btnL.Hide();
+                btnL.Width = 50;
+                btnL.Height = 34;
+
+                btnM.Hide();
+                btnM.Width = 50;
+                btnM.Height = 34;
+
+                btnN.Hide();
+                btnN.Width = 50;
+                btnN.Height = 34;
+
+                btnO.Hide();
+                btnO.Width = 50;
+                btnO.Height = 34;
+
+                btnP.Hide();
+                btnP.Width = 50;
+                btnP.Height = 34;
+
+                btnQ.Hide();
+                btnQ.Width = 50;
+                btnQ.Height = 34;
+
+                btnR.Hide();
+                btnR.Width = 50;
+                btnR.Height = 34;
+
+                btnS.Hide();
+                btnS.Width = 50;
+                btnS.Height = 34;
+
+                btnT.Hide();
+                btnT.Width = 50;
+                btnT.Height = 34;
+
+                btnU.Hide();
+                btnU.Width = 50;
+                btnU.Height = 34;
+
+                btnV.Hide();
+                btnV.Width = 50;
+                btnV.Height = 34;
+
+                btnW.Hide();
+                btnW.Width = 50;
+                btnW.Height = 34;
+
+                btnX.Hide();
+                btnX.Width = 50;
+                btnX.Height = 34;
+
+                btnY.Hide();
+                btnY.Width = 50;
+                btnY.Height = 34;
+
+                btnZ.Hide();
+                btnZ.Width = 50;
+                btnZ.Height = 34;
+                */
+            }
+            
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -33,24 +202,117 @@ namespace shapesMaker
             g.DrawArc(myPen, 400, 100, 50, 50, 0, 315);
             */
 
-            g.DrawArc(myPen, 100, 100, 250, 250, 0, 360);
-            g.DrawArc(myPen, 135, 130, 60, 60, 0, 360);
-            g.DrawArc(myPen, 255, 130, 60, 60, 0, 360);
-            g.DrawArc(myPen, 150, 180, 160, 130, 0, 180);
+            g.DrawLine(myPen, 50, 100, 50, 600);
+            g.DrawLine(myPen, 50, 100, 270, 100);
+            g.DrawLine(myPen, 50, 190, 140, 100);
+            g.DrawLine(myPen, 270, 100, 270, 180);
+            g.DrawArc(myPen, 230, 180, 80, 80, 0, 360);
+            g.DrawLine(myPen, 270, 260, 270, 400);
+            g.DrawLine(myPen, 270, 260, 220, 340);
+            g.DrawLine(myPen, 270, 260, 320, 340);
+            g.DrawLine(myPen, 270, 400, 220, 480);
+            g.DrawLine(myPen, 270, 400, 320, 480);
+            g.DrawLine(myPen, 245, 200, 260, 215);
+            g.DrawLine(myPen, 245, 215, 260, 200);
+            g.DrawLine(myPen, 295, 215, 280, 200);
+            g.DrawLine(myPen, 295, 200, 280, 215);
 
 
+            Title(e);
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNewWord_Load(object sender, EventArgs e)
+        {
             
-            String drawString = "yo!";
+        }
 
-            Font drawFont = new Font("Jokerman", 24);
 
-            SolidBrush drawBrush = new SolidBrush(Color.Green);
+        private void btnNewWord_Click(object sender, EventArgs e)
+        {
 
-            PointF drawPoint = new PointF(195.0F, 40.0F);
+        }
 
+        private void lblScore_Load(object sender, EventArgs e)
+        {
+            lblScore.Hide();
+            if (start = true)
+            {
+                lblScore.Text = "Score: " + score;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            start = true;
+            btnInstructions.Hide();
+            btnStart.Hide();
+
+            lblScore.Show();
+            btnNewWord.Show();
+            LetterButtonsShow();
+
+        }
+
+        private void btnInstructions_Click(object sender, EventArgs e)
+        {
+
+            start = true;
+            btnInstructions.Hide();
+            btnStart.Top = 580;
+            btnStart.Left = 400;
+
+
+        }
+
+
+        private void LetterButtonsShow()
+        {
+            btnA.Show();
+            btnB.Show();
+            btnC.Show();
+            btnD.Show();
+            btnE.Show();
+            btnF.Show();
+            btnG.Show();
+            btnH.Show();
+            btnI.Show();
+            btnJ.Show();
+            btnK.Show();
+            btnL.Show();
+            btnM.Show();
+            btnN.Show();
+            btnO.Show();
+            btnP.Show();
+            btnQ.Show();
+            btnR.Show();
+            btnS.Show();
+            btnT.Show();
+            btnU.Show();
+            btnV.Show();
+            btnW.Show();
+            btnX.Show();
+            btnY.Show();
+            btnZ.Show();
+        }
+
+
+        void Title(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Black);
+
+            String drawString = "HANGMAN";
+            Font drawFont = new Font("", 60, FontStyle.Underline);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            PointF drawPoint = new PointF(318.0F, 25.0F);
             g.DrawString(drawString, drawFont, drawBrush, drawPoint);
-
-
         }
     }
 }
